2348c27240f5a0a27624be6e95db680d76be4d8e
xiaowen <62494620+xiaowen-king@users.noreply.github.com>
2021-04-21 22:09:25 +0800

    Add files via upload

---
+++ 源码存放/第三章源码/3.1.1.py
@@ -0,0 +1,5 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
---
+++ 源码存放/第三章源码/3.1.2.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles[3])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1])
