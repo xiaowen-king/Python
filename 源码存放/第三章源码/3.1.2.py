5574b56eda19b83ee5daac9b8e7a3437f94788a2
xiaowen <62494620+xiaowen-king@users.noreply.github.com>
2021-04-21 22:10:15 +0800

    Delete 3.1.2.py

--- 源码存放/第三章源码/3.1.2.py
+++
@@ -1,6 +0,0 @@
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[1])
-print(bicycles[3])
-
-bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[-1])
